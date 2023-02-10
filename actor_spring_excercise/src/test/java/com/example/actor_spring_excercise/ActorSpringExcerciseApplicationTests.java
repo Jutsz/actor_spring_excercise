c5563d7462aac282ef8677b240410d196c879071
Jutsz <judit.fulmer@gmail.com>
2023-02-10 10:57:43 +0100

    add files

---
+++ actor_spring_excercise/src/test/java/com/example/actor_spring_excercise/ActorSpringExcerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actor_spring_excercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActorSpringExcerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
