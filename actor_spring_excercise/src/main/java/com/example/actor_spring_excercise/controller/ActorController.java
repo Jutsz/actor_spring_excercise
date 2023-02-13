3e586184fcc60e84fd4925e046c8774ebaffafdb
adamfilep <adamfilep@gmail.com>
2023-02-13 09:28:24 +0100

    Add main page

--- actor_spring_excercise/src/main/java/com/example/actor_spring_excercise/controller/ActorController.java
+++ actor_spring_excercise/src/main/java/com/example/actor_spring_excercise/controller/ActorController.java
@@ -3,6 +3,8 @@ package com.example.actor_spring_excercise.controller;
 import com.example.actor_spring_excercise.service.ActorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,8 +13,13 @@ public class ActorController {
     private ActorService actorService;
 
     @Autowired
-
     public ActorController(ActorService actorService) {
         this.actorService = actorService;
+    }
+
+    @GetMapping
+    public String actors(Model model) {
+        model.addAttribute("actors", actorService.getActors());
+        return "index";
     }
 }
--- actor_spring_excercise/src/main/java/com/example/actor_spring_excercise/service/ActorService.java
+++ actor_spring_excercise/src/main/java/com/example/actor_spring_excercise/service/ActorService.java
@@ -1,9 +1,12 @@
 package com.example.actor_spring_excercise.service;
 
 
+import com.example.actor_spring_excercise.model.Actor;
 import com.example.actor_spring_excercise.repository.ActorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ActorService {
@@ -11,5 +14,9 @@ public class ActorService {
     @Autowired
     public ActorService(ActorRepository actorRepository) {
         this.actorRepository = actorRepository;
+    }
+
+    public List<Actor> getActors() {
+        return actorRepository.getActors();
     }
 }
